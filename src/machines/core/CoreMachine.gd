06408e4510995fd676262d1be3d397e457d8f53e
Monczak <37554206+Monczak@users.noreply.github.com>
2023-12-30 02:40:28 +0100

    Add rudimentary action generation

--- src/machines/core/CoreMachine.gd
+++ src/machines/core/CoreMachine.gd
@@ -3,8 +3,11 @@ extends Node2D
 
 @export var chat_queue : NeuroActionQueue
 @onready var _execute_action_timer : Timer = $ExecuteActionTimer
+@onready var _new_action_timer : Timer = $NewActionTimer
 
 @export var execute_action_interval: float = 3.2
+
+@export var new_action_interval: float = 3.2    # TODO: Make this dependent on viewer count?
 
 
 func _input(event):
@@ -20,8 +23,42 @@ func _input(event):
 
 
 func _ready():
+    randomize()
     _execute_action_timer.timeout.connect(handle_planned_action)
     _execute_action_timer.start(execute_action_interval)
+
+    _new_action_timer.timeout.connect(func(): add_random_planned_action(); _new_action_timer.start(new_action_interval))
+    _new_action_timer.start(new_action_interval)
+
+    add_hi_action()
+
+
+func add_hi_action() -> void:
+    var hi_action = NeuroLogic.NeuroPlannedAction.new()
+    hi_action.origin = NeuroLogic.NeuroActionOrigin.Neuro
+    hi_action.category = NeuroLogic.NeuroActionCategory.HiChat
+    add_planned_action(hi_action)
+
+
+func add_random_planned_action() -> void:
+    var origin = randi() % len(NeuroLogic.NeuroActionOrigin.keys()) as NeuroLogic.NeuroActionOrigin
+
+    var categories = [ 
+        NeuroLogic.NeuroActionCategory.PogStuff, 
+        NeuroLogic.NeuroActionCategory.AboutHerself, 
+        NeuroLogic.NeuroActionCategory.InterestingStuff, 
+        NeuroLogic.NeuroActionCategory.Joke, 
+        NeuroLogic.NeuroActionCategory.Story, 
+        NeuroLogic.NeuroActionCategory.CorpaMoment, 
+        NeuroLogic.NeuroActionCategory.Question, 
+        NeuroLogic.NeuroActionCategory.Answer
+    ]
+    var category = categories[randi() % len(categories)]
+
+    var action = NeuroLogic.NeuroPlannedAction.new()
+    action.origin = origin
+    action.category = category
+    add_planned_action(action)
 
 
 func add_planned_action(action: NeuroLogic.NeuroPlannedAction) -> void:
