78196521c8d3b479be499062ed86c5d5d4ddd24e
MetRiko <18maximus8050@gmail.com>
2023-12-31 19:46:28 +0100

    Add tutorial

--- src/tutorial/TutorialCloud.gd
+++ src/tutorial/TutorialCloud.gd
@@ -1,11 +1,6 @@
 extends Control
 
-@export var machine_name : String = "MACHINE_NAME" 
-@export var machine_desc : String = "MACHINE_DESC" 
-
-func _ready():
-	$Panel/VBoxContainer/Name.text = machine_name
-	$Panel/VBoxContainer/Desc.text = machine_desc
+@export var panel : Control
 
 func _is_mouse_inside() -> bool:
 	var rect = get_global_rect()
@@ -14,13 +9,13 @@ func _is_mouse_inside() -> bool:
 
 func _process(delta):
 	if visible == false or is_visible_in_tree() == false or modulate.a == 0:
-		$Panel.visible = false
+		panel.visible = false
 		return
 	
 	var hovered := _is_mouse_inside()
 	$Sprite2D.frame = 1 if hovered else 0
 	
 	if hovered and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		$Panel.visible = true
+		panel.visible = true
 	else:
-		$Panel.visible = false
+		panel.visible = false
